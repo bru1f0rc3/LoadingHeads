7e309e4dcb813aa51c9562eeaf2932b5aeaaee03
___ <___@HOME-PC>
2024-02-08 13:01:30 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Net;
+using System.IO;
+using System.Threading;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = "head.txt"; 
+
+            try
+            {
+                string[] usernames = File.ReadAllLines(filePath); // Считывания никнеймов с файла head.txt
+
+                foreach (string username in usernames)
+                {
+                    DownloadHeadImage(username);
+                    Thread.Sleep(500);
+                }
+
+                Console.WriteLine("Скачивание завершено.");
+            }
+            catch (FileNotFoundException)
+            {
+                Console.WriteLine($"Файл не найден: {filePath}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Произошла ошибка: {ex.Message}");
+            }
+        }
+
+        static void DownloadHeadImage(string username)
+        {
+            var heads = $"https://skin.vimeworld.ru/helm/3d/{username}.png";
+
+            try
+            {
+                Console.WriteLine($"Загрузка изображения для пользователя {username}...");
+                var downloadsPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), "Downloads");
+
+                var saveFolderPath = Path.Combine(downloadsPath, "heads");
+                Directory.CreateDirectory(saveFolderPath);
+
+                var savePath = Path.Combine(saveFolderPath, $"{username}.png");
+
+                using (var webClient = new WebClient())
+                {
+                    webClient.DownloadFile(heads, savePath);
+                }
+
+                Console.WriteLine($"Изображение для пользователя {username} успешно скачано.");
+                Console.WriteLine($"Путь сохранения: {savePath}");
+            }
+            catch (WebException ex)
+            {
+                Console.WriteLine($"Ошибка при загрузке изображения для пользователя {username}: {ex.Message}");
+            }
+            catch (IOException ex)
+            {
+                Console.WriteLine($"Ошибка при сохранении изображения для пользователя {username}: {ex.Message}");
+            }
+        }
+    }
+}
